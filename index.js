5ba1b2f94173d4b8ebc5f3f986b39832dabbe7a7
paragsewani <142333190+paragsewani@users.noreply.github.com>
2024-05-06 21:23:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+
+var headingload=document.getElementsByClassName(".page1 h1");
+function loadinganimation(headingload){
+ for(var i=0;i<1;i++){
+  var headelement=document.getElementsByClassName(".page1 .heading");
+ opacity:0;
+ duration:0.2;
+ delay:0.2;
+
+ } 
+  
+
+}
+loadinganimation();
+  
+ 
+
